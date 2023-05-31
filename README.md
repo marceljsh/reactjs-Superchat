19cb111d64abbaf9aa4a7482ad3f4955e97b8dd2
Marcel Joshua <marceljoshua69@gmail.com>
2023-05-31 19:51:35 +0000

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# reactjs-Superchat-by-Fireship
-A simple real time chat app built on React.js and Firebase
+# Superchat
+
+## `Overview🏝`
+
+A simple real time chat app built on React.js and Firebase.
+
+<br>
+
+## `Tech Stacks🔧`
+[React.js](https://react.dev), [Firebase](https://firebase.google.com)
+
+<br>
+
+## `How to use🤔`
+1. open the app and authenticate yourself by signing in via Google
+<br>
+<img src="./images/sign-in.png" title="sign in" alt="sign in" width="500" />
+<br>
+<br>
+
+2. enjoy!
+<br>
+<img src="./images/chat-room.png" title="chat room" alt="chat room" width="500" />
+<br>
+<br>
+
+## `License📜`
+Copyright © 2023, [Marcel Joshua](github.com/marceljsh) and [Fireship](https://www.youtube.com/@Fireship).
+***
+_This property is intended as a self exploration requirement_
---
+++ src/App.js
@@ -0,0 +1,115 @@
+import React, { useRef, useState } from 'react';
+import './App.css';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-whvsirjgRXS-6ukP-ZXqXN1RsbmrpR0",
+  authDomain: "superchat-cffa7.firebaseapp.com",
+  projectId: "superchat-cffa7",
+  storageBucket: "superchat-cffa7.appspot.com",
+  messagingSenderId: "259662808961",
+  appId: "1:259662808961:web:7436459afe279702d94a80",
+  measurementId: "G-EJGXXSMJY6"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+const SignIn = () => {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  );
+};
+
+const SignOut = () => {
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  );
+};
+
+const ChatMessage = (props) => {
+  const { text, uid, photoURL } = props.message;
+
+  const isCurrentUser = uid === auth.currentUser.uid;
+  const messageClass = isCurrentUser ? 'sent' : 'received';
+
+  console.log(`should be: ${text}`);
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} width='50px' />
+      <p>{text}</p>
+    </div>
+  )
+};
+
+const ChatRoom = () => {
+  const dummy = useRef();
+  const messageRef = firestore.collection('messages');
+  const query = messageRef.orderBy('createdAt');
+  
+  const [messages] = useCollectionData(query, {idField: 'id'});
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async(e) => {
+    e.preventDefault();
+    
+    const { uid, photoURL } = auth.currentUser;
+
+    await messageRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    });
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <>
+      <main>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+        <div ref={dummy}></div>
+      </main>
+
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
+        <button type="submit">🕊️</button>
+      </form>
+    </>
+  );
+};
+
+const App = () => {
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Superchat⚛️🦸🏻‍♂️💬</h1>
+        <SignOut />
+      </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+    </div>
+  );
+}
+
+export default App;
